35d89807a1bce8c7b2140311b69bb33ad8b12993
Edgar <eduranm@dgb.unam.mx>
2025-02-05 02:19:21 -0600

    Ajusta directorio de descarga para el modelo llama

--- llama3/download_model.py
+++ llama3/download_model.py
@@ -5,8 +5,10 @@ HF_TOKEN = 'INTRODUCE_TOKEN'
 
 login(token=HF_TOKEN)
 
+LLAMA_MODEL_DIR = "llama3/llama-3.2"
+MODEL_LLAMA = "llama-3.2-3b-instruct-q4_k_m.gguf"
 repo_id = 'hugging-quants/Llama-3.2-3B-Instruct-Q4_K_M-GGUF'
-filename = 'llama-3.2-3b-instruct-q4_k_m.gguf'
-local_dir = 'llama-3.2'
+filename = MODEL_LLAMA
+local_dir = LLAMA_MODEL_DIR
 
 downloaded_file = hf_hub_download(repo_id=repo_id, filename=filename, local_dir=local_dir)
