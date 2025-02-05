58c93845ef6890347b84c7c82bb2ae8aca07cd45
Edgar <eduranm@dgb.unam.mx>
2025-02-05 03:07:26 -0600

    Agrega modelos para referencias

---
+++ reference/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+from core.models import (
+    CommonControlField,
+)
+from wagtail.models import Orderable
+from wagtail.admin.panels import FieldPanel, InlinePanel
+from modelcluster.fields import ParentalKey
+from modelcluster.models import ClusterableModel
+from django.utils.translation import gettext as _
+from django.core.validators import MinValueValidator, MaxValueValidator
+from core.forms import CoreAdminModelForm
+
+# Create your models here
+class Reference(CommonControlField, ClusterableModel):
+    mixed_citation = models.TextField(("Mixed Citation"), null=False, blank=True)
+
+    estatus = models.IntegerField(default=0) 
+
+    panels = [
+        FieldPanel('mixed_citation'),
+        InlinePanel('element_citation')
+    ]
+
+    base_form_class = CoreAdminModelForm
+
+    def __str__(self):
+        return self.mixed_citation
+
+
+class ElementCitation(Orderable):
+    reference = ParentalKey(
+        Reference, on_delete=models.SET_NULL, related_name="element_citation", null=True
+    )
+    marked = models.JSONField(_("Marked"), default=dict, blank=True)
+
+    score = models.IntegerField(
+        null=True, 
+        blank=True,
+        validators=[
+            MinValueValidator(1),  # Mínimo 1
+            MaxValueValidator(10)  # Máximo 10
+        ],
+        help_text="Rating from 1 to 10"
+    )
+
+    panels = [
+            FieldPanel("marked"),
+            FieldPanel("score"),
+            ]
