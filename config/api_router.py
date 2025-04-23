9fdb0714d2b2f26af1fa15799e43384e0760fa9c
Edgar <eduranm@dgb.unam.mx>
2025-04-23 09:52:14 -0600

    Configuracion de la ruta para la api

---
+++ config/api_router.py
@@ -0,0 +1,15 @@
+from django.conf import settings
+from rest_framework.routers import DefaultRouter, SimpleRouter
+
+from reference.api.v1.views import ReferenceViewSet
+
+app_name = "reference"
+
+if settings.DEBUG:
+    router = DefaultRouter()
+else:
+    router = SimpleRouter()
+
+router.register("reference", ReferenceViewSet, basename="reference")
+
+urlpatterns = router.urls
