e781091e22c3ab6af9d820240284d9649b55c076
Pitanga Innovare <rafael@pitangainnovare.com.br>
2025-08-25 15:21:10 -0300

    exclui exceções não usadas e cria outras

--- tracker/exceptions.py
+++ tracker/exceptions.py
@@ -1,26 +1,6 @@
-class ProcEventCreateError(Exception):
+class GeneralEventCreateError(Exception):
     ...
 
 
-class UnexpectedEventCreateError(Exception):
-    ...
-
-
-class EventCreateError(Exception):
-    ...
-
-
-class EventReportCreateError(Exception):
-    ...
-
-
-class EventReportSaveFileError(Exception):
-    ...
-
-
-class EventReportCreateError(Exception):
-    ...
-
-
-class EventReportDeleteEventsError(Exception):
+class XMLDocumentEventCreateError(Exception):
     ...
