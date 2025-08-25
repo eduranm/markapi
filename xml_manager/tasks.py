8404fa5a6e2b3f571c4a988cacd131b2ddc4a560
Pitanga Innovare <rafael@pitangainnovare.com.br>
2025-08-25 15:24:40 -0300

    melhora tasks e implementa processamento de xml para pdf

--- xml_manager/tasks.py
+++ xml_manager/tasks.py
@@ -3,14 +3,30 @@ import os
 import tempfile
 
 from packtools import data_checker
+from packtools.sps.formats.pdf.pipeline import docx
+from packtools.sps.formats.pdf.utils import file_utils
+from packtools.sps.utils import xml_utils
+from django.core.files import File
+from django.conf import settings
 
 from django.contrib.auth import get_user_model
 from django.utils.translation import gettext as _
 
 from config import celery_app
-from core.utils.utils import _get_user
+from tracker.choices import (
+    XML_DOCUMENT_PARSING_ERROR,
+    XML_DOCUMENT_VALIDATION_ERROR,
+    XML_DOCUMENT_CONVERSION_TO_DOCX_ERROR,
+    XML_DOCUMENT_CONVERSION_TO_PDF_ERROR,
+    XML_DOCUMENT_CONVERSION_TO_TEX_ERROR,
+)
+from tracker.models import XMLDocumentEvent
 
-from .models import XMLDocument
+from .models import (
+    XMLDocument, 
+    XMLDocumentHTML, 
+    XMLDocumentPDF,
+)
 
 
 User = get_user_model()
@@ -45,47 +61,119 @@ def task_process_xml_document(self, xml_id, user_id=None, username=None):
 @celery_app.task(bind=True, timelimit=-1)
 def task_validate_xml_file(self, xml_id, user_id=None, username=None):
     try:
-        xml_file = XMLDocument.objects.get(id=xml_id)
+        xml_document = XMLDocument.objects.get(id=xml_id)
     except XMLDocument.DoesNotExist:
         logging.error(f'XML file with ID {xml_id} does not exist.')
         return False
     
     user = _get_user(self.request, username=username, user_id=user_id)
     
-    logging.info(f'Starting XML validation for XML file {xml_file.xml_file.name}.')
+    logging.info(f'Starting XML validation for XML file {xml_document.xml_file.name}.')
     params = {}
 
-    temp_dir = tempfile.gettempdir()
-    base_filename = os.path.splitext(os.path.basename(xml_file.xml_file.name))[0]
+    base_filename = os.path.splitext(os.path.basename(xml_document.xml_file.name))[0]
 
-    path_csv = os.path.join(temp_dir, f"{base_filename}.validation.csv")
-    path_exceptions = os.path.join(temp_dir, f"{base_filename}.exceptions.json")
+    validation_dir = os.path.join(settings.MEDIA_ROOT, 'xml_manager', 'validation')
+    exceptions_dir = os.path.join(settings.MEDIA_ROOT, 'xml_manager', 'validation')
+    os.makedirs(validation_dir, exist_ok=True)
+    os.makedirs(exceptions_dir, exist_ok=True)
+
+    path_csv = os.path.join(validation_dir, f"{base_filename}.validation.csv")
+    path_exceptions = os.path.join(exceptions_dir, f"{base_filename}.exceptions.json")
 
     try:
-        validator = data_checker.XMLDataChecker(path_csv, path_exceptions, xml_file.xml_file.path)
+        validator = data_checker.XMLDataChecker(path_csv, path_exceptions, xml_document.xml_file.path)
         validator.validate(params=params, csv_per_xml=False)
     except Exception as e:
         logging.error(f'Error during XML validation: {e}')
+        XMLDocumentEvent.create(
+            xml_document=xml_document,
+            error_type=XML_DOCUMENT_VALIDATION_ERROR,
+            data={},
+            message=str(e),
+            save=True,
+        )
         return False
     
-    xml_file.validation_file = path_csv
-    xml_file.exceptions_file = path_exceptions
-    xml_file.save()
-    logging.info(f'XML validation completed successfully for {xml_file.xml_file.name}.')
+    with open(path_csv, 'rb') as f_csv:
+        xml_document.validation_file.save(os.path.basename(path_csv), File(f_csv), save=False)
+
+    with open(path_exceptions, 'rb') as f_exc:
+        xml_document.exceptions_file.save(os.path.basename(path_exceptions), File(f_exc), save=False)
+
+    xml_document.save()
+    logging.info(f'XML validation completed successfully for {xml_document.xml_file.name}.')
+    return True
 
 
 @celery_app.task(bind=True, timelimit=-1)
 def task_generate_pdf_file(self, xml_id, user_id=None, username=None):    
     try:
-        xml_file = XMLDocument.objects.get(id=xml_id)
+        xml_document = XMLDocument.objects.get(id=xml_id)
     except XMLDocument.DoesNotExist:
         logging.error(f'XML file with ID {xml_id} does not exist.')
         return False
     
     user = _get_user(self.request, username=username, user_id=user_id)
-    
-    logging.info(f'Starting PDF generation for XML file {xml_file.xml_file.name}.')
-    # TODO: Implement PDF generation logic here
+
+    try:
+        xml_tree = xml_utils.get_xml_tree(xml_document.xml_file.path)
+    except Exception as e:
+        logging.error(f'Error parsing XML file: {e}')
+        XMLDocumentEvent.create(
+            xml_document=xml_document,
+            error_type=XML_DOCUMENT_PARSING_ERROR,
+            data={},
+            message=str(e),
+            save=True,
+        )
+        return False
+
+    # TODO: Iterate over available languages
+
+    logging.info(f'Starting PDF generation for XML file {xml_document.xml_file.name}.')
+    try:
+        base_docx_layout = os.path.join(settings.MEDIA_ROOT, 'layout.docx')
+        document = docx.pipeline_docx(xml_tree, data={'base_layout': base_docx_layout})
+    except Exception as e:
+        logging.error(f'Error during PDF generation: {e}')
+        XMLDocumentEvent.create(
+            xml_document=xml_document,
+            error_type=XML_DOCUMENT_CONVERSION_TO_DOCX_ERROR,
+            data={},
+            message=str(e),
+            save=True,
+        )
+        return False
+
+    pdf_dir = os.path.join(settings.MEDIA_ROOT, 'xml_manager', 'pdf')
+    os.makedirs(pdf_dir, exist_ok=True)
+
+    # DOCX intermediário
+    temp_dir = tempfile.gettempdir()
+    base_filename = os.path.splitext(os.path.basename(xml_document.xml_file.name))[0]
+    path_intermediate_docx = os.path.join(temp_dir, f"{base_filename}.docx")
+    document.save(path_intermediate_docx)
+
+    path_pdf = os.path.join(pdf_dir, f"{base_filename}.pdf")
+    try:
+        file_utils.convert_docx_to_pdf(path_intermediate_docx, path_pdf)
+    except Exception as e:
+        logging.error(f'Error during DOCX to PDF conversion: {e}')
+        XMLDocumentEvent.create(
+            xml_document=xml_document,
+            error_type=XML_DOCUMENT_CONVERSION_TO_PDF_ERROR,
+            data={},
+            message=str(e),
+            save=True,
+        )
+        return False
+
+    with open(path_pdf, 'rb') as f_pdf:
+        pdf_instance = XMLDocumentPDF(xml_document=xml_document, language="pt")
+        pdf_instance.pdf_file.save(os.path.basename(path_pdf), File(f_pdf), save=True)
+
+    return True
 
 
 @celery_app.task(bind=True, timelimit=-1)
