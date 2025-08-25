5b48f5da1aa964efb0041778c09fb7104f555ae8
Pitanga Innovare <rafael@pitangainnovare.com.br>
2025-08-25 15:24:06 -0300

    melhora wagtail hooks para apresentar os modelos de documentxml*

--- xml_manager/wagtail_hooks.py
+++ xml_manager/wagtail_hooks.py
@@ -1,21 +1,54 @@
+import os
+
 from django.urls import path, include
+from django.urls import reverse
+from django.utils.html import format_html
 from django.utils.translation import gettext_lazy as _
 from wagtail import hooks
-from wagtail.admin.menu import MenuItem
+from wagtail.admin.ui.tables import Column
 from wagtail.snippets.models import register_snippet
-from wagtail.snippets.views.snippets import SnippetViewSet
+from wagtail.snippets.views.snippets import SnippetViewSet, SnippetViewSetGroup
 
 from config.menu import get_menu_order
 
-from .models import XMLDocument
+from .models import XMLDocument, XMLDocumentPDF, XMLDocumentHTML
 from . import urls
 
 
-@hooks.register('register_admin_urls')
-def register_admin_urls():
-    return [
-        path('xml-manager/', include(urls)),
-    ]
+class FileNameColumn(Column):
+    """
+    Coluna que mostra apenas o nome do arquivo de um FileField.
+    """
+    def get_value(self, instance):
+        val = super().get_value(instance)
+        if not val:
+            return "-"
+        return os.path.basename(getattr(val, "name", str(val)))
+
+
+class LinkColumn(Column):
+    """
+    Column que recebe um FileField (FieldFile) e renderiza um <a href>.
+    Cria '-' se não houver arquivo.
+    """
+    def get_value(self, instance):
+        val = super().get_value(instance)
+        if not val:
+            return "-"
+        name = os.path.basename(getattr(val, "name", str(val)))
+        try:
+            url = val.url
+        except Exception:
+            return name
+        return format_html('<a href="{}" target="_blank">{}</a>', url, name)
+
+
+class ActionColumn(Column):
+    def get_value(self, instance):
+        url = reverse("process_xml_pk", args=[instance.pk])
+        return format_html(
+            '<a href="{}" class="button button-small">Processar</a>', url
+        )
 
 
 class XMLDocumentSnippetViewSet(SnippetViewSet):
@@ -26,30 +59,73 @@ class XMLDocumentSnippetViewSet(SnippetViewSet):
     menu_name = "xml_manager"
     menu_label = _("XML Document")
     menu_order = get_menu_order("xml_manager")
-    add_to_admin_menu = True
+    add_to_admin_menu = False
 
     list_display = (
         "xml_file",
-        "validation_file",
-        "exceptions_file",
-        "pdf_file",
-        "html_file",
+        LinkColumn("validation_file", label=_("Validation file")),
+        LinkColumn("exceptions_file", label=_("Exceptions file")),
         "uploaded_at",
+        ActionColumn("actions", label=_("Ações")),
     )
-    list_filter = (
+
+    search_fields = ("xml_file",)
+
+
+class XMLDocumentPDFSnippetViewSet(SnippetViewSet):
+    model = XMLDocumentPDF
+    verbose_name = _("XML Document PDF")
+    verbose_name_plural = _("XML Document PDFs")
+    icon = "doc-full"
+    menu_name = "xml_manager"
+    menu_label = _("XML Document PDF")
+    menu_order = get_menu_order("xml_manager")
+    add_to_admin_menu = False
+
+    list_display = (
+        "xml_document",
+        "pdf_file",    
     )
-    search_fields = (
-        "xml_file",
+
+    search_fields = ("pdf_file",)
+
+
+class XMLDocumentHTMLSnippetViewSet(SnippetViewSet):
+    model = XMLDocumentHTML
+    verbose_name = _("XML Document HTML")
+    verbose_name_plural = _("XML Document HTMLs")
+    icon = "doc-full"
+    menu_name = "xml_manager"
+    menu_label = _("XML Document HTML")
+    menu_order = get_menu_order("xml_manager")
+    add_to_admin_menu = False
+
+    list_display = (
+        "xml_document",
+        "html_file",    
+    )
+
+    search_fields = ("html_file",)
+
+
+
+class XMLDocumentSnippetViewSetGroup(SnippetViewSetGroup):
+    menu_name = 'xml_manager'
+    menu_label = _("XML Manager")
+    menu_icon = "folder-open-inverse"
+    menu_order = get_menu_order("xml_manager")
+    items = (
+        XMLDocumentSnippetViewSet,
+        XMLDocumentHTMLSnippetViewSet,
+        XMLDocumentPDFSnippetViewSet,
     )
 
 
-register_snippet(XMLDocumentSnippetViewSet)
+register_snippet(XMLDocumentSnippetViewSetGroup)
 
-@hooks.register('register_admin_menu_item')
-def register_process_form_menu_item():
-    return MenuItem(
-        _('Process XML'),
-        '/admin/xml-manager/upload/',
-        icon_name='form',
-        order=get_menu_order("xml_manager") + 1,
-    )
+
+@hooks.register('register_admin_urls')
+def register_admin_urls():
+    return [
+        path('xml-manager/', include(urls)),
+    ]
