e03184576ad13903220a3bb6dbf02ac004ddc42e
Pitanga Innovare <rafael@pitangainnovare.com.br>
2025-08-25 15:22:50 -0300

    melhora forma de processar XML - remove views/templates desnecessários

--- xml_manager/urls.py
+++ xml_manager/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 
 urlpatterns = [
-    path('upload/', views.upload_xml_page, name='upload_xml_page'),
-    path('process/', views.process_xml, name='process_xml'),
+    path("process/<int:pk>/", views.process_xml_pk, name="process_xml_pk"),
 ]
--- xml_manager/views.py
+++ xml_manager/views.py
@@ -1,27 +1,14 @@
-from django.shortcuts import render, redirect
-from django.urls import reverse
-from django.views.decorators.csrf import csrf_exempt
-from django.views.decorators.http import require_POST
 from django.contrib import messages
+from django.contrib.admin.views.decorators import staff_member_required
+from django.shortcuts import redirect, get_object_or_404
 
 from .tasks import task_process_xml_document
 from .models import XMLDocument
 
 
-def upload_xml_page(request):
-    return render(request, "xml_manager/process_form.html")
-
-
-@require_POST
-def process_xml(request):
-    xml_file = request.FILES.get('xml_file')
-    if not xml_file:
-        messages.error(request, "Arquivo não enviado.")
-        return redirect(reverse('upload_xml_page'))
-
-    xml_obj = XMLDocument.objects.create(xml_file=xml_file)
-
-    task_process_xml_document.delay(xml_obj.id)
-
-    messages.success(request, "Arquivo enviado para processamento!")
-    return redirect(reverse('upload_xml_page'))
+@staff_member_required
+def process_xml_pk(request, pk):
+    obj = get_object_or_404(XMLDocument, pk=pk)
+    task_process_xml_document.delay(obj.id)
+    messages.success(request, f"XML {obj.id} enviado para processamento")
+    return redirect("/admin/snippets/xml_manager/xmldocument/")
