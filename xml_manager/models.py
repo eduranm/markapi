318ab055a9f6c5b23d4ae8125d4194ec84f30495
Pitanga Innovare <rafael@pitangainnovare.com.br>
2025-08-24 23:27:48 -0300

    Cria modelos XMLDocumentPDF e XMLDocumentHTML e altera XMLDocument

--- xml_manager/models.py
+++ xml_manager/models.py
@@ -20,20 +20,38 @@ class XMLDocument(models.Model):
         null=True,
         verbose_name=_("Exceptions File")
     )
-    pdf_file = models.FileField(
-        upload_to='xml_manager/pdf/',
-        blank=True,
-        null=True,
-        verbose_name=_("PDF File")
-    )
-    html_file = models.FileField(
-        upload_to='xml_manager/html/',
-        blank=True,
-        null=True,
-        verbose_name=_("HTML File")
-    )
     uploaded_at = models.DateTimeField(
         auto_now_add=True,
         verbose_name=_("Uploaded At"),
         help_text=_("The date and time when the file was uploaded.")
     )
+
+
+class XMLDocumentPDF(models.Model):
+    xml_document = models.ForeignKey(XMLDocument, on_delete=models.CASCADE, related_name="pdfs", verbose_name=_("XML Document"))
+    pdf_file = models.FileField(
+        upload_to='xml_manager/pdf/',
+        verbose_name=_("PDF File")
+    )
+    language = models.CharField(
+        max_length=32,
+        default="pt",
+        verbose_name=_("Language"),
+        help_text=_("Language code or name")
+    )
+    uploaded_at = models.DateTimeField(auto_now_add=True, verbose_name=_("Uploaded At"))
+
+
+class XMLDocumentHTML(models.Model):
+    xml_document = models.ForeignKey(XMLDocument, on_delete=models.CASCADE, related_name="htmls", verbose_name=_("XML Document"))
+    html_file = models.FileField(
+        upload_to='xml_manager/html/',
+        verbose_name=_("HTML File")
+    )
+    language = models.CharField(
+        max_length=32,
+        default="pt",
+        verbose_name=_("Language"),
+        help_text=_("Language code or name")
+    )
+    uploaded_at = models.DateTimeField(auto_now_add=True, verbose_name=_("Uploaded At"))
