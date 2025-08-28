764b54cba499075c77b52744f1279e3bb633ec0e
Pitanga Innovare <rafael@pitangainnovare.com.br>
2025-08-27 21:04:32 -0300

    refatora tasks para usar utilitário novo e tratar erros com detalhes

--- xml_manager/tasks.py
+++ xml_manager/tasks.py
@@ -66,20 +66,16 @@ def task_validate_xml_file(self, xml_id, user_id=None, username=None):
     logging.info(f'Starting XML validation for XML file {xml_document.xml_file.name}.')
     params = {}
 
-    base_filename = os.path.splitext(os.path.basename(xml_document.xml_file.name))[0]
-
-    validation_dir = os.path.join(settings.MEDIA_ROOT, 'xml_manager', 'validation')
-    exceptions_dir = os.path.join(settings.MEDIA_ROOT, 'xml_manager', 'validation')
-    os.makedirs(validation_dir, exist_ok=True)
-    os.makedirs(exceptions_dir, exist_ok=True)
-
-    path_csv = os.path.join(validation_dir, f"{base_filename}.validation.csv")
-    path_exceptions = os.path.join(exceptions_dir, f"{base_filename}.exceptions.json")
-
     try:
-        validator = data_checker.XMLDataChecker(path_csv, path_exceptions, xml_document.xml_file.path)
-        validator.validate(params=params, csv_per_xml=False)
-    except Exception as e:
+        path_csv, path_exceptions = utils.validate_xml_document(
+            xml_document.xml_file.path,
+            output_root_dir=os.path.join(settings.MEDIA_ROOT, 'xml_manager', 'validation'),
+            params=params,
+        )
+        xml_document.validation_file.name = os.path.relpath(path_csv, settings.MEDIA_ROOT)
+        xml_document.exceptions_file.name = os.path.relpath(path_exceptions, settings.MEDIA_ROOT)
+        xml_document.save()
+    except exceptions.XML_File_Validation_Error as e:
         logging.error(f'Error during XML validation: {e}')
         XMLDocumentEvent.create(
             xml_document=xml_document,
@@ -89,20 +85,24 @@ def task_validate_xml_file(self, xml_id, user_id=None, username=None):
             save=True,
         )
         return False
-    
-    with open(path_csv, 'rb') as f_csv:
-        xml_document.validation_file.save(os.path.basename(path_csv), File(f_csv), save=False)
+    except Exception as e:
+        logging.error(f'Unexpected error during XML validation: {e}')
+        XMLDocumentEvent.create(
+            xml_document=xml_document,
+            error_type=XML_DOCUMENT_UNKNOWN_ERROR,
+            data={},
+            message=str(e),
+            save=True,
+        )
+        return False
 
-    with open(path_exceptions, 'rb') as f_exc:
-        xml_document.exceptions_file.save(os.path.basename(path_exceptions), File(f_exc), save=False)
-
-    xml_document.save()
     logging.info(f'XML validation completed successfully for {xml_document.xml_file.name}.')
+
     return True
 
 
 @celery_app.task(bind=True, timelimit=-1)
-def task_generate_pdf_file(self, xml_id, user_id=None, username=None):    
+def task_generate_pdf_file(self, xml_id, libreoffice_binary='libreoffice', user_id=None, username=None):    
     try:
         xml_document = XMLDocument.objects.get(id=xml_id)
     except XMLDocument.DoesNotExist:
@@ -111,10 +111,23 @@ def task_generate_pdf_file(self, xml_id, user_id=None, username=None):
     
     user = _get_user(self.request, username=username, user_id=user_id)
 
+    params = {'libreoffice_binary': libreoffice_binary,}
+
+    logging.info(f'Starting PDF generation for XML file {xml_document.xml_file.name}.')
     try:
-        xml_tree = xml_utils.get_xml_tree(xml_document.xml_file.path)
-    except Exception as e:
-        logging.error(f'Error parsing XML file: {e}')
+        path_pdf, path_docx, lang = utils.generate_pdf_for_xml_document(
+            xml_document.xml_file.path,
+            output_root_dir=os.path.join(settings.MEDIA_ROOT, 'xml_manager', 'pdf'),
+            params=params,
+        )
+
+        pdf_instance = XMLDocumentPDF(xml_document=xml_document, language=lang)
+        pdf_instance.pdf_file.name = os.path.relpath(path_pdf, settings.MEDIA_ROOT)
+        pdf_instance.docx_file.name = os.path.relpath(path_docx, settings.MEDIA_ROOT)
+        pdf_instance.save()
+
+    except exceptions.XML_File_Parsing_Error as e:
+        logging.error(f'Error during XML parsing: {e}')
         XMLDocumentEvent.create(
             xml_document=xml_document,
             error_type=XML_DOCUMENT_PARSING_ERROR,
@@ -122,16 +135,9 @@ def task_generate_pdf_file(self, xml_id, user_id=None, username=None):
             message=str(e),
             save=True,
         )
-        return False
 
-    # TODO: Iterate over available languages
-
-    logging.info(f'Starting PDF generation for XML file {xml_document.xml_file.name}.')
-    try:
-        base_docx_layout = os.path.join(settings.MEDIA_ROOT, 'layout.docx')
-        document = docx.pipeline_docx(xml_tree, data={'base_layout': base_docx_layout})
-    except Exception as e:
-        logging.error(f'Error during PDF generation: {e}')
+    except exceptions.XML_File_DOCX_Generation_Error as e:
+        logging.error(f'Error during XML to DOCX conversion: {e}')
         XMLDocumentEvent.create(
             xml_document=xml_document,
             error_type=XML_DOCUMENT_CONVERSION_TO_DOCX_ERROR,
@@ -139,22 +145,9 @@ def task_generate_pdf_file(self, xml_id, user_id=None, username=None):
             message=str(e),
             save=True,
         )
-        return False
 
-    pdf_dir = os.path.join(settings.MEDIA_ROOT, 'xml_manager', 'pdf')
-    os.makedirs(pdf_dir, exist_ok=True)
-
-    # DOCX intermediário
-    temp_dir = tempfile.gettempdir()
-    base_filename = os.path.splitext(os.path.basename(xml_document.xml_file.name))[0]
-    path_intermediate_docx = os.path.join(temp_dir, f"{base_filename}.docx")
-    document.save(path_intermediate_docx)
-
-    path_pdf = os.path.join(pdf_dir, f"{base_filename}.pdf")
-    try:
-        file_utils.convert_docx_to_pdf(path_intermediate_docx, path_pdf)
-    except Exception as e:
-        logging.error(f'Error during DOCX to PDF conversion: {e}')
+    except exceptions.XML_File_PDF_Generation_Error as e:
+        logging.error(f'Error during PDF generation: {e}')
         XMLDocumentEvent.create(
             xml_document=xml_document,
             error_type=XML_DOCUMENT_CONVERSION_TO_PDF_ERROR,
@@ -162,11 +155,17 @@ def task_generate_pdf_file(self, xml_id, user_id=None, username=None):
             message=str(e),
             save=True,
         )
-        return False
 
-    with open(path_pdf, 'rb') as f_pdf:
-        pdf_instance = XMLDocumentPDF(xml_document=xml_document, language="pt")
-        pdf_instance.pdf_file.save(os.path.basename(path_pdf), File(f_pdf), save=True)
+    except Exception as e:
+        logging.error(f'Unexpected error during PDF generation: {e}')
+        XMLDocumentEvent.create(
+            xml_document=xml_document,
+            error_type=XML_DOCUMENT_UNKNOWN_ERROR,
+            data={},
+            message=str(e),
+            save=True,
+        )
+        return False
 
     return True
 
@@ -174,12 +173,48 @@ def task_generate_pdf_file(self, xml_id, user_id=None, username=None):
 @celery_app.task(bind=True, timelimit=-1)
 def task_generate_html_file(self, xml_id, user_id=None, username=None):    
     try:
-        xml_file = XMLDocument.objects.get(id=xml_id)
+        xml_document = XMLDocument.objects.get(id=xml_id)
     except XMLDocument.DoesNotExist:
         logging.error(f'XML file with ID {xml_id} does not exist.')
         return False
     
     user = _get_user(self.request, username=username, user_id=user_id)
     
-    logging.info(f'Starting HTML generation for XML file {xml_file.xml_file.name}.')
-    # TODO: Implement HTML generation logic here
+    logging.info(f'Starting HTML generation for XML file {xml_document.xml_file.name}.')
+    try:
+        path_html, lang = utils.generate_html_for_xml_document(
+            xml_document.xml_file.path,
+            output_root_dir=os.path.join(settings.MEDIA_ROOT, 'xml_manager', 'html'),
+            config=settings.HTML_GENERATION_CONFIG,
+        )
+        html_instance = XMLDocumentHTML(xml_document=xml_document, language=lang)
+        html_instance.html_file.name = os.path.relpath(path_html, settings.MEDIA_ROOT)
+        html_instance.save()
+    except exceptions.XML_File_Parsing_Error as e:
+        logging.error(f'Error during XML parsing: {e}')
+        XMLDocumentEvent.create(
+            xml_document=xml_document,
+            error_type=XML_DOCUMENT_PARSING_ERROR,
+            data={},
+            message=str(e),
+            save=True,
+        )
+    except exceptions.XML_File_HTML_Generation_Error as e:
+        logging.error(f'Error during HTML generation: {e}')
+        XMLDocumentEvent.create(
+            xml_document=xml_document,
+            error_type=XML_DOCUMENT_CONVERSION_TO_HTML_ERROR,
+            data={},
+            message=str(e),
+            save=True,
+        )
+    except Exception as e:
+        logging.error(f'Unexpected error during HTML generation: {e}')
+        XMLDocumentEvent.create(
+            xml_document=xml_document,
+            error_type=XML_DOCUMENT_UNKNOWN_ERROR,
+            data={},
+            message=str(e),
+            save=True,
+        )
+        return False
