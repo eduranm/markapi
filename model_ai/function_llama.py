4944e3bf62ac9d7f6132c5b02a2f7607da73ddbf
Edgar <eduranm@dgb.unam.mx>
2025-09-26 10:37:44 -0600

    Nueva app model_ai: integración de LLaMA, funciones genéricas, mensajes de inferencia, tareas y hooks de Wagtail

---
+++ model_ai/function_llama.py
@@ -0,0 +1,21 @@
+from llama_cpp import Llama
+import os
+from model_ai import messages
+
+class functionsLlama:
+
+    def getFirstMetadata(text):
+        print(messages.ALL_FIRST_BLOCK.format(text=text))
+        return messages.ALL_FIRST_BLOCK.format(text=text)
+
+    def getDoiAndSection():
+        return messages.DOI_AND_SECTION_MESSAGES, messages.DOI_AND_SECTION_FORMAT
+
+    def getTitles():
+        return messages.TITLE_MESSAGES, messages.TITLE_RESPONSE_FORMAT
+
+    def getAuthorConfig():
+        return messages.AUTHOR_MESSAGES, messages.AUTHOR_RESPONSE_FORMAT
+    
+    def getAffiliations():
+        return messages.AFFILIATION_MESSAGES, messages.AFFILIATION_RESPONSE_FORMAT
---
+++ model_ai/tasks.py
@@ -0,0 +1,23 @@
+from config import celery_app
+from model_ai.models import LlamaModel, DownloadStatus
+from config.settings.base import LLAMA_MODEL_DIR
+
+from huggingface_hub import login
+from huggingface_hub import hf_hub_download
+
+
+def get_model(hf_token, name_model, name_file):
+    login(token=hf_token)
+    local_dir = LLAMA_MODEL_DIR
+    downloaded_file = hf_hub_download(repo_id=name_model, filename=name_file, local_dir=local_dir)
+
+
+@celery_app.task()
+def download_model(id):
+    try:
+        instance = LlamaModel.objects.get(id=id)
+        get_model(instance.hf_token, instance.name_model, instance.name_file)
+        instance.download_status = DownloadStatus.DOWNLOADED
+    except:
+        instance.download_status = DownloadStatus.ERROR
+    instance.save()
